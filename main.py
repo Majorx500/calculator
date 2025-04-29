55bd5567e2c551d3b5fe2eb0c3786e68fbc54f0d
Majorx500 <majorx500@gmail.com>
2025-04-29 14:00:13 +0200

    aaa

--- main.py
+++ main.py
@@ -1,45 +1,27 @@
 from tkinter import *
 from tkinter import ttk
 
-def add1():
-    equation.set(equation.get() + "1")
-def add2():
-    equation.set(equation.get() + "2")
-def add3():
-    equation.set(equation.get() + "3")
-def add4():
-    equation.set(equation.get() + "4")
-def add5():
-    equation.set(equation.get() + "5")
-def add6():
-    equation.set(equation.get() + "6")
-def add7():
-    equation.set(equation.get() + "7")
-def add8():
-    equation.set(equation.get() + "8")
-def add9():
-    a = equation.get()
-    equation.set(a+"9")
-def add0():
-    equation.set(equation.get() + "0")
-def addplus():
-    equation.set(equation.get() +"+")
-def addminus():
-    equation.set(equation.get() +"-")
-def adddiv():
-    equation.set(equation.get() +"/")
-def addmult():
-    equation.set(equation.get() +"*")
+def addchar(symbol):
+    res.set(res.get() + str(symbol))
+    return
+
 def calculate(*args):
     try:
-        val = eval(equation.get())
+        val = eval(res.get())
         res.set(val)
-        clear()
     except ValueError:
+        res.set("Value Error")
+        clear()
+        pass
+    except ZeroDivisionError:
+        res.set("Can't divide by 0")
+        clear()
+        pass
+    except SyntaxError:
         pass
 def clear(*args):
     try:
-        equation.set(" ")
+        res.set(" ")
     except ValueError:
         pass
 
@@ -53,21 +35,22 @@ equation = StringVar()
 res = StringVar()
 ttk.Label(mainframe,textvariable=equation).grid(column=0,row=0)
 ttk.Label(mainframe,textvariable=res).grid(column=2,row=0)
-ttk.Button(mainframe, text = "1",command=add1).grid(column=0,row=1)
-ttk.Button(mainframe, text = "2",command=add2).grid(column=1,row=1)
-ttk.Button(mainframe, text = "3",command=add3).grid(column=2,row=1)
-ttk.Button(mainframe, text = "4",command=add4).grid(column=0,row=2)
-ttk.Button(mainframe, text = "5",command=add5).grid(column=1,row=2)
-ttk.Button(mainframe, text = "6",command=add6).grid(column=2,row=2)
-ttk.Button(mainframe, text = "7",command=add7).grid(column=0,row=3)
-ttk.Button(mainframe, text = "8",command=add8).grid(column=1,row=3)
-ttk.Button(mainframe, text = "9",command=add9).grid(column=2,row=3)
-ttk.Button(mainframe, text = "0",command=add0).grid(column=1,row=4)
-ttk.Button(mainframe, text = "+",command=addplus).grid(column=3,row=1)
-ttk.Button(mainframe, text = "-",command=addminus).grid(column=3,row=2)
-ttk.Button(mainframe, text = "/",command=adddiv).grid(column=3,row=3)
-ttk.Button(mainframe, text = "*",command=addmult).grid(column=3,row=4)
+ttk.Button(mainframe, text = "1",command=lambda:addchar('1')).grid(column=0,row=1)
+ttk.Button(mainframe, text = "2",command=lambda:addchar('2')).grid(column=1,row=1)
+ttk.Button(mainframe, text = "3",command=lambda:addchar('3')).grid(column=2,row=1)
+ttk.Button(mainframe, text = "4",command=lambda:addchar('4')).grid(column=0,row=2)
+ttk.Button(mainframe, text = "5",command=lambda:addchar('5')).grid(column=1,row=2)
+ttk.Button(mainframe, text = "6",command=lambda:addchar('6')).grid(column=2,row=2)
+ttk.Button(mainframe, text = "7",command=lambda:addchar('7')).grid(column=0,row=3)
+ttk.Button(mainframe, text = "8",command=lambda:addchar('8')).grid(column=1,row=3)
+ttk.Button(mainframe, text = "9",command=lambda:addchar('9')).grid(column=2,row=3)
+ttk.Button(mainframe, text = "0",command=lambda:addchar('0')).grid(column=1,row=4)
+ttk.Button(mainframe, text = "+",command=lambda:addchar('+')).grid(column=3,row=1)
+ttk.Button(mainframe, text = "-",command=lambda:addchar('-')).grid(column=3,row=2)
+ttk.Button(mainframe, text = "/",command=lambda:addchar('/')).grid(column=3,row=3)
+ttk.Button(mainframe, text = "*",command=lambda:addchar('*')).grid(column=3,row=4)
 ttk.Button(mainframe, text = "=",command=calculate).grid(column=2,row=4)
+ttk.Button(mainframe,text="C", command=clear).grid(column=0,row=4)
 for child in mainframe.winfo_children():
     child.grid_configure(padx=5,pady=5)
 
